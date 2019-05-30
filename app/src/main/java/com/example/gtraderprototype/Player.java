7a45195469e7764a2461266e943b587aeca57117
tylerwannamaker <twannamaker1@student.gsu.edu>
2019-05-30 11:59:20 -0400

    Firebase and Android Studio Initialization

---
+++ app/src/main/java/com/example/gtraderprototype/Player.java
@@ -0,0 +1,24 @@
+package com.example.gtraderprototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Player {
+    public String name;
+    public int difficulty;
+    public Map<String, Double> skillSet = new HashMap<>();
+
+    public Player(String name, int difficulty){
+        this.name = name;
+        this.difficulty = difficulty;
+    }
+    public void addSkill(String skillName, double skillValue){
+        if(skillSet.get(skillName)!=null){
+            skillSet.remove(skillName);
+        }
+        skillSet.put(skillName, skillValue);
+
+    }
+
+}
+
