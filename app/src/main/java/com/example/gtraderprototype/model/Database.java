66eebb307226676080009d146fa6413f8f1aecbc
tylerwannamaker <37714246+tylerwannamaker@users.noreply.github.com>
2019-06-14 00:26:55 -0400

    DB Save States & Instances Init

---
+++ app/src/main/java/com/example/gtraderprototype/model/Database.java
@@ -0,0 +1,61 @@
+package com.example.gtraderprototype.model;
+import android.util.Log;
+
+import com.example.gtraderprototype.entity.GameInstance;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class Database {
+    /*
+        This class communicates with the firebase database
+     */
+
+    static FirebaseDatabase database = FirebaseDatabase.getInstance();
+    static DatabaseReference gameIDListRef = database.getReference("Save States");
+
+    public Database(){
+
+
+    }
+    public static String getNewGameID(){
+
+                String tempID;
+                    tempID = "";
+                    for(int i=0;i<16;i++){
+                        int key = (int)(Math.floor(Math.random()*10));
+                        tempID+=(char)((int)'a'+key);
+                    }
+        Log.d("GTrader", "Key "+tempID+" generated.");
+                return tempID;
+
+    }
+    public static void saveState(GameInstance instance){
+        DatabaseReference stateref = database.getReference("Save States/"+instance.getGameID());
+        stateref.setValue(instance);
+    }
+
+    public static void retrieveGameFromDB(String gameID){
+        DatabaseReference ref = database.getReference("Save States/"+gameID);
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                /*
+                Post post = dataSnapshot.getValue(Post.class);
+                System.out.println(post);
+                 */
+                Log.d("GTraderDB", dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+    }
+
+
+}
