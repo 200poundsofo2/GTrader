d5e72175dbcd6071625f4d437f204ee693622683
Yaro <ysboyko@gmail.com>
2019-06-22 15:09:30 -0400

    Adjusted for save page

---
+++ app/src/main/java/com/example/gtraderprototype/views/SavedGamesActivity.java
@@ -0,0 +1,18 @@
+package com.example.gtraderprototype.views;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.gtraderprototype.R;
+
+public class SavedGamesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_games);
+    }
+}
