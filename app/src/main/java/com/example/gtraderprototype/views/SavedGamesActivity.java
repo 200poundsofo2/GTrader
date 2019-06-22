7c24413d4ef8fb82f6b41f0a60c46b1bd9a33989
Yaro <ysboyko@gmail.com>
2019-06-22 15:39:27 -0400

    Added more functionality to Saved Games
    Note: instead of adding button for each saved games, lets add a method that changes the color of the button when it has a saved game vs doesnt. Also, we would have to change each instance. We could also possibly change the saved game name...

--- app/src/main/java/com/example/gtraderprototype/views/SavedGamesActivity.java
+++ app/src/main/java/com/example/gtraderprototype/views/SavedGamesActivity.java
@@ -5,14 +5,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.gtraderprototype.R;
 
 public class SavedGamesActivity extends AppCompatActivity {
 
+    private Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_games);
+        back = findViewById(R.id.back);
+    }
+
+    public void backToWelcome(View view){
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SavedGamesActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
