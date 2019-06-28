faece91b04977c4ebb207a05bb05cdcb5a8d6c63
zoecq001 <zoecq001@gmail.com>
2019-06-28 00:57:04 -0400

    econ model

--- app/src/main/java/com/example/gtraderprototype/entity/IndustrialGood.java
+++ app/src/main/java/com/example/gtraderprototype/entity/IndustrialGood.java
@@ -1,21 +1,49 @@
 package com.example.gtraderprototype.entity;
 
-enum IndustrialGood implements TradeGood{
+import java.util.HashSet;
+import java.util.Random;
 
-    games(180),firearms(725),medicine(510),narcotics(2625),robots(3950 );
+enum TradeGood{
+
+    water(30,0,0,3,4,30,50,Condition.DROUGHT, Condition.LOTSOFWATER,Condition.DESERT),
+    furs(250,0,0,10,10, 230, 280, Condition.COLD, Condition.RICHFAUNA,Condition.LIFELESS),
+    food(100,1,0,5,5,90,160, Condition.CROPFAIL,Condition.RICHSOIL,Condition.POORSOIL),
+    ore(350,2,2,20,10,350,420,Condition.WAR,Condition.MINERALRICH,Condition.MINERALPOOR),
+
+    games(250,3,1,-10,5,160,270,Condition.BOREDOM,Condition.ARTISTIC,null),
+    firearms(1250,3,1,-75,100,600,1100,Condition.WAR,Condition.WARLIKE,null),
+    medicine(650,4,1,-20,10,400,700,Condition.PLAGUE, Condition.LOTSOFHERBS,null),
+    machines(900,4,3,-30,5,600,800,Condition.LACKOFWORKERS,null,null),
+    narcotics(3500,5,0,-125,150,2000,3000,Condition.BOREDOM,Condition.WEIRDMUSHROOMS,null),
+    robots(5000,6,4,-150,100,3500,5000,Condition.LACKOFWORKERS,null,null );
     //the basic credit, may vary according to tech level and circumstances
 
-    private int credit;
+    private int basePrice; //the basic credit, may vary according to tech level and circumstances
     private String owner;
+    private int MTLP; //Minimum Tech Level to Produce this resource
+    private int MTLU; //Minimum Tech Level to Use this resource
+    private int IPL; //Price increase per tech level
+    private int Var; //variance is the maximum percentage that the price can vary above or below the base
+    private int MTL; //Min price offered in space trade with random trader (not on a region)
+    private int MTH; //Max price offered in space trade with random trader (not on a region)
+    private Condition IE; // Radical price increase event
+    private Condition CR; //condition is present, the price of this resource is unusually low
+    private Condition ER; //this condition is present, the resource is expensive
 
-    IndustrialGood(int credit){
-        this.credit = credit;
+    TradeGood(int basePrice, int MTLP, int MTLU, int IPL, int Var, int MTL, int MTH, Condition IE, Condition CR, Condition ER){
+        this.basePrice = basePrice;
+        this.MTLP = MTLP;
+        this.MTLU = MTLU;
+        this.IPL = IPL;
+        this.Var=Var;
+        this.MTL = MTL;
+        this.MTH = MTH;
+        this.IE = IE;
+        this.CR =CR;
+        this.ER=ER;
     }
-    public int getCredit(){
-        return credit;
-    }
-    public void setCredit(){
-        this.credit = credit;
+    public int getbasePrice(){
+        return basePrice;
     }
     public String getOwner(){
         return owner;
@@ -24,13 +52,29 @@ enum IndustrialGood implements TradeGood{
         this.owner=owner;
     }
 
-    @Override
-    public void adjustCreditTechLevel(){
-       //("to be implemented later");
+    public int getRegionPrice(HashSet<Condition> conditionSet, TechLevel techLevel){
+        int price = basePrice+IPL*(techLevel.getLevel()-MTLP);
+        Random rand=new Random();
+        int variance=rand.nextInt(Var+1)*basePrice;
+        if(rand.nextBoolean()){
+            price+=variance;
+        }else{
+            price-=variance;
+        }
+        if(conditionSet.contains(IE)){
+            price*=1.2;
+        }
+        if(conditionSet.contains(CR)){
+            price*=0.8;
+        }
+        if(conditionSet.contains(ER)){
+            price*=4;
+        }
+        return price;
     }
 
-    @Override
-    public void adjustCreditCircumstance(){
-        //("to be implemented later");
+    public int getRandomTraderPrice(){
+        Random random=new Random();
+        return MTL+random.nextInt(MTH+1);
     }
 }
--- app/src/main/java/com/example/gtraderprototype/entity/Region.java
+++ app/src/main/java/com/example/gtraderprototype/entity/Region.java
@@ -6,11 +6,22 @@ import com.example.gtraderprototype.model.Database;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+import java.util.HashSet;
 
 enum TechLevel{
-    PRE_AGRICULTURE, AGRICULTURE, MEDIEVAL, RENAISSANCE, EARLY_INDUSTRIAL, INDUSTRIAL, POST_INDUSTRIAL, HI_TECH;
+    PRE_AGRICULTURE(0), AGRICULTURE(1), MEDIEVAL(2), RENAISSANCE(3), EARLY_INDUSTRIAL(4), INDUSTRIAL(5), POST_INDUSTRIAL(6), HI_TECH(7);
+
+    private int level;
+    TechLevel(int level){
+        this.level=level;
+    }
+
+    public int getLevel(){
+        return level;
+    }
 
     public static TechLevel getRandomLevel(){
         final List<TechLevel> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
@@ -19,6 +30,7 @@ enum TechLevel{
         return VALUES.get(RANDOM.nextInt(SIZE));
     }
 }
+
 enum Resources{
     NO_SPECIAL_RESOURCES, MINERAL_RICH, MINERAL_POOR, DESERT, LOTS_OF_WATER, RICH_SOIL, POOR_SOIL, RICH_FAUNA, LIFELESS, WEIRD_MUSHROOMS, LOTS_OF_HERBS, ARTISTIC, WARLIKE;
 
@@ -29,6 +41,13 @@ enum Resources{
         return VALUES.get(RANDOM.nextInt(SIZE));
     }
 }
+
+enum Condition{
+    DROUGHT,LOTSOFWATER,DESERT,COLD,RICHFAUNA,LIFELESS,CROPFAIL,RICHSOIL,POORSOIL,WAR,MINERALRICH,MINERALPOOR,BOREDOM,ARTISTIC,WARLIKE,PLAGUE,LOTSOFHERBS,
+    LACKOFWORKERS,WEIRDMUSHROOMS;
+
+}
+
 public class Region {
     public String getRegionName() {
         return regionName;
@@ -62,12 +81,23 @@ public class Region {
         this.resources = resources;
     }
 
+    public void addCondition(Condition condition){
+        conditionSet.add(condition);
+    }
+
+    public HashSet<Condition> getConditionSet() {
+        return conditionSet;
+    }
+
     //public Police[] police;
     //public Trader[] traders;
     public String regionName;
     public int[] coordinates;
     public TechLevel techLevel;
     public Resources resources;
+
+    private HashSet<Condition> conditionSet;
+
     public Region(){
         this.regionName = Database.getRandomName();
         this.coordinates = new int[]{(int)(Math.random()*90), (int)(Math.random()*90)};
