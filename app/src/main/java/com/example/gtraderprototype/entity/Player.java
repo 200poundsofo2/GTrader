90c5301ac553e025c41c9dc548155f55a69d4289
Yaro <ysboyko@gmail.com>
2019-06-28 20:36:44 -0400

    Added marketplace page functionality

--- app/src/main/java/com/example/gtraderprototype/entity/Player.java
+++ app/src/main/java/com/example/gtraderprototype/entity/Player.java
@@ -3,6 +3,7 @@ package com.example.gtraderprototype.entity;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Player { //singleton design pattern: Player.getPlayer()
     public static volatile Player player = null;
@@ -12,18 +13,20 @@ public class Player { //singleton design pattern: Player.getPlayer()
     private int engineerSkillPoints;
     private int fighterSkillPoints;
     private int traderSkillPoints;
-    private List<String> Inventory;
+    private List<String> inventory;
     private boolean isPirate;
     private int money;
     private String spaceship;
     private String currentLocation;
 
 
-    public Player(){}
-    public static Player getPlayer(){
-        if (player == null){
-            synchronized (Player.class){
-                if(player == null){
+    public Player() {
+    }
+
+    public static Player getPlayer() {
+        if (player == null) {
+            synchronized (Player.class) {
+                if (player == null) {
                     player = new Player();
                 }
             }
@@ -31,7 +34,7 @@ public class Player { //singleton design pattern: Player.getPlayer()
         return player;
     }
 
-    public Player (String name, int pilotPoints, int engineerPoints, int fighterPoints, int traderPoints){
+    public Player(String name, int pilotPoints, int engineerPoints, int fighterPoints, int traderPoints) {
         this.name = name;
         this.pilotSkillPoints = pilotPoints;
         this.engineerSkillPoints = engineerPoints;
@@ -39,28 +42,51 @@ public class Player { //singleton design pattern: Player.getPlayer()
         this.traderSkillPoints = traderPoints;
         this.money = 1000;
         this.spaceship = "Gnat";
+        this.inventory = new ArrayList<String>();
+        this.inventory.add("Pulse Laser");
     }
 
     public void setName(String name) {
         getPlayer().name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public int getPilotSkillPoints(){return pilotSkillPoints;}
-    public int getEngineerSkillPoints(){return  engineerSkillPoints;}
-    public int getFighterSkillPoints(){return fighterSkillPoints;}
-    public int getTraderSkillPoints(){return traderSkillPoints;}
 
-    public void setPirate(boolean pirate){
-        getPlayer().isPirate=pirate;
+    public int getPilotSkillPoints() {
+        return pilotSkillPoints;
     }
-    public boolean getIsPirate(){
+
+    public int getEngineerSkillPoints() {
+        return engineerSkillPoints;
+    }
+
+    public int getFighterSkillPoints() {
+        return fighterSkillPoints;
+    }
+
+    public int getTraderSkillPoints() {
+        return traderSkillPoints;
+    }
+
+    public List<String> getInventory() {
+        return inventory;
+    }
+
+    public void setPirate(boolean pirate) {
+        getPlayer().isPirate = pirate;
+    }
+
+    public boolean getIsPirate() {
         return isPirate;
     }
-    public String toString(){return "Player Name: " + name + ", Pilot Skill Points: " + pilotSkillPoints +
-            ", Engineer Skill Points: " + engineerSkillPoints + ", Fighter Skill Points: " + fighterSkillPoints +
-            ", Trader Skill Points: " + traderSkillPoints +
-            ", Money: " + money + ", SpaceShip: "+ spaceship;}
+
+    public String toString() {
+        return "Player Name: " + name + ", Pilot Skill Points: " + pilotSkillPoints +
+                ", Engineer Skill Points: " + engineerSkillPoints + ", Fighter Skill Points: " + fighterSkillPoints +
+                ", Trader Skill Points: " + traderSkillPoints +
+                ", Money: " + money + ", SpaceShip: " + spaceship;
+    }
 }
 
