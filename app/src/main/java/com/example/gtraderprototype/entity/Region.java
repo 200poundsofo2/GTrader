6d85cec40c971cc7d258f575e11b514e7b326cab
Christian Holmes <mr.xcrom@aol.com>
2019-07-17 01:49:42 -0400

    tet fixes

--- app/src/main/java/com/example/gtraderprototype/entity/Region.java
+++ app/src/main/java/com/example/gtraderprototype/entity/Region.java
@@ -10,20 +10,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.HashSet;
 
-enum TechLevel{
-    PRE_AGRICULTURE(0), AGRICULTURE(1), MEDIEVAL(2), RENAISSANCE(3), EARLY_INDUSTRIAL(4), INDUSTRIAL(5), POST_INDUSTRIAL(6), HI_TECH(7);
-    private int techLevel;
-    public  int getTechLevel(){ return techLevel;}
-    TechLevel (int techLevel){
-        this.techLevel = techLevel;
-    }
-    public static TechLevel getRandomLevel(){
-        final List<TechLevel> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
-        int SIZE = VALUES.size();
-        final Random RANDOM = new Random();
-        return VALUES.get(RANDOM.nextInt(SIZE));
-    }
-}
+
 public class Region {
     public String getRegionName() {
         return regionName;
